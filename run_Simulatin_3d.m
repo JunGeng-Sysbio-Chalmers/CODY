232a45a1c6acc9979405b0ce29587d86f69b9c2b
JunGeng-Sysbio-Chalmers <47929951+JunGeng-Sysbio-Chalmers@users.noreply.github.com>
2019-08-16 11:57:17 +0200

    Update run_Simulatin_3d.m

--- run_Simulatin_3d.m
+++ run_Simulatin_3d.m
@@ -1,18 +1,25 @@
-function Result_total=run_Simulation_3d(cohort,index)
+function Result_total=run_Simulation_3d(cohort,index,filename)
+
+% filemet=dir('*.jpg')
+% regexp(filemet,'Metabolite_*.jpg','match')
+% if exist('Metabolite_*.jpg')
 
 index_matlab=index;            
                  
 if strcmpi(cohort,'Infant')
     Result_BF='20181024_4m_diet_302h.mat';
     Result_SF='20181029_12m_diet_732h.mat';
-    Result_total=concate_4m_12m_mets_3d(Result_BF,Result_SF,index_matlab);
-                
+    Result_total=concate_4m_12m_mets_3d(Result_BF,Result_SF,index_matlab,filename);
+%      Result_total=concate_4m_12m_mets_3d_4_server(Result_BF,Result_SF,index_matlab);
+
     
 elseif strcmpi(cohort,'Adult')
 
     Result_BF='20190312_CM_diet_baseline.mat';
     Result_SF='20190319_CM_diet_intervention.mat';
-    Result_total=concate_4m_12m_mets_3d_adult(Result_BF,Result_SF,index_matlab);
+    Result_total=concate_4m_12m_mets_3d_adult(Result_BF,Result_SF,index_matlab,filename);
+%     Result_total=concate_4m_12m_mets_3d_adult_4_server(Result_BF,Result_SF,index_matlab);
+
                  
                  
 end
